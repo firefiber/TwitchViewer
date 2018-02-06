ff7b913a876a5658c2cce26669f606cc21c4c80b
Shan W <shan.w@outlook.com>
2018-02-06 07:59:20 +0400

    Skeleton files + directory structure. Commented script for API endpoints.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//ClientID: mmsj66xshsa6xtk024r136cawnweyw
+//Twitch API endpoint: https://api.twitch.tv/kraken
+
+  //List:
+
+  //Streams: /streams
+  //Featured: /streams/featured
+  //Limits: ?limit=??&offsent=??
+  //Live: &stream_type=live
