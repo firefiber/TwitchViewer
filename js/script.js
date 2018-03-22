e5b70ca01e111df0cf87497b4d03e75dbb2020c0
Shan W <shan.w@outlook.com>
2018-03-23 00:27:39 +0400

    Working vanilla request.

--- js/script.js
+++ js/script.js
@@ -7,3 +7,44 @@
   //Featured: /streams/featured
   //Limits: ?limit=??&offsent=??
   //Live: &stream_type=live
+
+//console.log("test");
+//$.ajax({
+//  type:'GET',
+//  url:'https://api.twitch.tv/helix/streams',
+//  dataType:'json',
+//  headers:  {
+//    'Client-ID':"mmsj66xshsa6xtk024r136cawnweyw"
+//  }
+//}).done(function(msg){
+//    console.log(msg);
+//});
+
+//function json(){
+        let endpoint = "https://api.twitch.tv/helix/streams";
+        let request = new XMLHttpRequest();
+
+        request.open('GET', endpoint);
+        request.setRequestHeader('Client-ID', "mmsj66xshsa6xtk024r136cawnweyw")
+        request.send();
+        request.responseType='json';
+
+        request.onload = function(){
+            let resp = request.response;
+            console.log(resp);
+        }
+//};
+
+
+//
+//
+//    let mapsRequest = new XMLHttpRequest();
+//
+//    mapsRequest.open('GET', gmapsURL+"&latlng="+lat+","+lon);
+//    mapsRequest.send();
+//    mapsRequest.responseType = 'json';
+//
+//    mapsRequest.onload = function(){
+//      locResp = mapsRequest.response;
+//      showLocation(locResp);
+//    }
