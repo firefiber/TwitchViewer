5002c677a7276d02a51be146e9da02827368930d
Shan W <shan.w@outlook.com>
2018-03-25 04:41:31 +0400

    Displays game name. Made top5 streams call. Displays all results on page. Started styles from mobile first. Super ugly.

--- js/script.js
+++ js/script.js
@@ -20,21 +20,17 @@
 
 const setList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 const endpoint = "https://api.twitch.tv/helix/";
-let userID_streams = [];
-let userID_users = [];
-let liveUsers = {};
+let userID_streams = []; //Stores stream details from api call [for "setList" users] - only returns streams that are live.
+let userID_users = []; //Stores user details from api call [for "setList" users].
 let request = new XMLHttpRequest();
 
+
 // ----------TEMPORARY THINGS
-let streams = document.querySelector('button#streams');
-let users = document.querySelector('button#users');
-//let link = document.querySelector('a');
-//streams.onclick = getStreams;
-//users.onclick = getUsers;
+
 // ----------TEMPORARY THINGS
 
 // Gets user information from set list of usernames - displays all users, regardless of live or offline.
-function getUsers() {
+function getUsers(userList) {
     const usernames = setList.map((elem) => {
         return `login=${elem}`;
     }).join("&");
@@ -46,15 +42,18 @@ function getUsers() {
     request.onload = function () {
         let resp = JSON.parse(request.response);
         if (resp.data.length > 0) {
-            resp.data.forEach(elem => userID_users.push(elem));
+            resp.data.forEach(elem => {
+                userID_users.push(elem)
+            });
+
         }
-//        console.log(resp, userID_users);
         getStreams();
     }
 }
 
 // Gets streams based on the set list of usersnames - only displays live streams.
-function getStreams() {
+function getStreams(userList) {
+    let gameIDs = [];
     const usernames = setList.map((elem) => {
         return `user_login=${elem}`;
     }).join("&");
@@ -64,45 +63,67 @@ function getStreams() {
     request.send();
 
     request.onload = function () {
-        let resp = JSON.parse(request.response);
-        if (resp.data.length > 0) {
-            resp.data.forEach(elem => userID_streams.push(elem));
-        }
-        console.log(userID_streams);
+        /* After it's loaded, it'll check if the response is empty:
+            If not, push each item into userID_streams, get all the 'game_id' values, put those into a string,
+            and call getGameNames with that string as the 'id' parameter. 'buildLinks' will be called from there.
+
+            If empty, call buildLinks.
+        */
+        let resp = JSON.parse(request.response).data;
+        if (resp.length > 0) {
+            resp.forEach(elem => {
+                userID_streams.push(elem);
+                gameIDs.push(elem.game_id)
+            });
+            const gameIDstring = gameIDs.map(elem => {
+                return `id=${elem}`;
+            }).join('&');
+            getGameName(gameIDstring);
+
+        } else buildLinks();
+
+    }
+}
+
+// Make API call to get game info based on given game ID's - gets 'name' property and stores in userID_streams.Calls 'buildLinks' when done.
+function getGameName(gameList) {
+    const reqType = 'games?';
+    request.open('GET', `${endpoint+reqType+gameList}`);
+    request.setRequestHeader('Client-ID', "mmsj66xshsa6xtk024r136cawnweyw");
+    request.send();
+
+    request.onload = function () {
+        let resp = JSON.parse(request.response).data;
+        resp.forEach(elem => {
+            for (let x of userID_streams) {
+                if (x.game_id == elem.id) {
+                    x.name = elem.name;
+                }
+            }
+        });
         buildLinks();
     }
 }
 
-// Finds out which users are live by comparing userIDs from 'streams' and 'users' - adds matching to `liveUsers` object.
-function checkLiveUsers() {
-    for (let a of userID_users) {
-        for (let b of userID_streams) {
-            if (a.id == b.user_id) {
-                liveUsers[`${a.display_name}`] = [a, b];
-                return true;
+//Creates all the elements to display results on the page.
+function buildLinks() {
+    let userList = document.querySelector('ul');
+    /*  Checks if current user [passed as param] is currently live by matching userID's from 'streams' and 'users'
+        For each user that's live, add a 'name' property to userID_stream with the name of the game being played. */
+    userID_users.forEach(elem => {
+        function checkIfLive(user) {
+            for (let key of userID_streams) {
+                if (key.user_id == user.id) {
+                    game.textContent = key.name;
+                    return true;
+                }
             }
         }
-    }
-}
-
-// Checks if current user [passed as param] is currently live
-function checkIfLive(user){
-    for(let key of userID_streams){
-        if(key.user_id == user.id){
-            return true;
-        }
-    }
-}
-
-// Creates links to each profile - opens in a new tab
-function buildLinks() {
-//    getStreams();
-    let userList = document.querySelector('ul');
-    userID_users.forEach(elem => {
         let listItem = document.createElement('li');
         let listLink = document.createElement('a');
         let userImg = document.createElement('img');
         let userStatus = document.createElement('p');
+        let game = document.createElement('p');
 
         listLink.href = `https://twitch.tv/${elem.login}`;
         listLink.textContent = `${elem.display_name}`;
@@ -110,20 +131,34 @@ function buildLinks() {
 
         userImg.src = `${elem.profile_image_url}`;
 
-        if(checkIfLive(elem)){
+        if (checkIfLive(elem)) {
             userStatus.textContent = 'Live';
-        } else{
+            console.log(Object.keys(userID_streams[0]));
+            console.log(Object.getOwnPropertyDescriptor(userID_streams[0], "name"));
+        } else {
             userStatus.textContent = 'Offline';
         }
 
-//        console.log(elem);
-
+        listItem.appendChild(userImg);
         listItem.appendChild(listLink);
         listItem.appendChild(userStatus);
-        listItem.appendChild(userImg);
+        listItem.appendChild(game);
         userList.appendChild(listItem);
     })
 }
+
+function getTop8(){
+    let reqType = `streams?first=8`;
+    request.open('GET', `${endpoint+reqType}`);
+    request.setRequestHeader('Client-ID', "mmsj66xshsa6xtk024r136cawnweyw");
+    request.send();
+
+    request.onload = function(){
+        let resp = JSON.parse(request.response).data;
+        console.log(resp);
+    }
+}
+
 
 (function () {
     getUsers();
@@ -132,6 +167,16 @@ function buildLinks() {
 
 
 
-
-
 //  Future: Add search functionality
+
+//// Finds out which users are live by comparing userIDs from 'streams' and 'users' - adds matching to `liveUsers` object.
+//function checkLiveUsers() {
+//    for (let a of userID_users) {
+//        for (let b of userID_streams) {
+//            if (a.id == b.user_id) {
+//                liveUsers[`${a.display_name}`] = [a, b];
+//                return true;
+//            }
+//        }
+//    }
+//}
